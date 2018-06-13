ed4c07c591c3008a66a94e2e3a1029216169bde0
Finn Terdal <fterdal@gmail.com>
2018-06-13 16:26:36 -0500

    simple demo

---
+++ demo.js
@@ -0,0 +1,22 @@
+console.clear()
+const { Client } = require('pg')
+const client = new Client({
+  connectionString: 'postgres://localhost/pg-demo'
+})
+
+const connect = async () => {
+  try {
+    await client.connect()
+    await client.query(`DROP TABLE IF EXISTS users`)
+    // console.log(await client.query('SELECT NOW()'))
+    console.log(await client.query(`
+      CREATE TABLE users (
+        id int,
+        email varchar(255)
+      )
+    `))
+  } catch (err) {
+    console.error(err)
+  }
+}
+connect()
